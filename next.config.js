eccd4ec17ea5bcd3edbf91909dee6c0065331de2
Anoosha Shetty <Anoosha.Shetty@Guidesly.com>
2023-04-15 11:39:59 +0530

    update packages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const withCSS = require('@zeit/next-css')
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   reactStrictMode: true,
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  }
 }
 
 module.exports = nextConfig
